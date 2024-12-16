d856a13fa3a3fa858ffb6483c8731327c4afc88d
rojored7 <83846540+rojored7@users.noreply.github.com>
2024-12-16 16:26:50 -0500

    red

---
+++ bubble.py
@@ -0,0 +1,1 @@
+python\ndef bubble_sort(arr):\n n = len(arr)\n for i in range(n):\n for j in range(0, n-i-1):\n if arr[j] > arr[j+1]:\n arr[j], arr[j+1] = arr[j+1], arr[j]\n return arr\n\n# Test de prueba\narr = [64, 34, 25, 12, 22, 11, 90]\nsorted_arr = bubble_sort(arr)\nprint(\"Arreglo ordenado:\", sorted_arr)\n```\n
